28c23f971ccd6b4399a028391ba7584860398ed2
Kent1an <qkent@anamosa.k12.ia.us>
2026-01-30 22:42:15 +0000

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+// Define the courseLength variable and assign it the value 20
+const courseLength = 20;
+
+// Assign the innerHTML of the element with id 'result' to courseLength
+document.getElementById('result').innerHTML = courseLength;
